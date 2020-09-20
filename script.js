d6a40264f25eb6265464eee64220d0c31656d1ae
StevanDow <thefamousunknown@gmail.com>
2020-09-20 20:41:13 +0200

    Scroll to top

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-var scroll = new SmoothScroll('a[href*="#"]');
+//Get the button
+var mybutton = document.getElementById("myBtn");
 
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-  } 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
