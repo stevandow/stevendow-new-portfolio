d4c322909588ffec71afd171b741d860cacb32ad
StevanDow <thefamousunknown@gmail.com>
2020-09-21 12:17:09 +0200

    ScrollTop btn

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-// Tooltip Script 
-$(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();
-});
+//Get the button:
+mybutton = document.getElementById("btn");
 
-// ScrollTop Script
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if($(this).scrollTop() > 20)
-        {
-            $("#btn").fadeIn();
-        }
-        else
-        {
-            $("#btn").fadeOut();
-        }
-    });
-});
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
